0ab33a48aedccdde133535dc77ddada9d28bc23e
yshong1998 <yshong1998@gmail.com>
2025-10-23 19:10:38 +0900

    initial commit

---
+++ src/test/java/com/moodeng/ezshop/EzShopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.moodeng.ezshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EzShopApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
