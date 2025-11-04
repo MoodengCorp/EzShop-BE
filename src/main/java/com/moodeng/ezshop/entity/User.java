f5b689b439e41a050a8a0660869b77cab16bba66
HiTae18 <somu950108@gmail.com>
2025-11-05 04:47:55 +0900

    Feat: User 엔터티 정의
    User 엔터티의 기본 필드와 제약조건 설정
    
    Closes: #4
    Ref: #2

--- src/main/java/com/moodeng/ezshop/entity/User.java
+++ src/main/java/com/moodeng/ezshop/entity/User.java
@@ -1,4 +1,51 @@
 package com.moodeng.ezshop.entity;
 
+import com.moodeng.ezshop.constant.Role;
+import jakarta.persistence.*;
+import lombok.*;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "users")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+
 public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "user_id")
+    private Long id;
+
+    @Column(name = "email", nullable = false, unique = true, length = 100)
+    private String email;
+
+    @Column(name = "password", nullable = false, length = 255)
+    private String password;
+
+    @Column(name = "name", nullable = false, length = 50)
+    private String name;
+
+    @Column(name = "phone", nullable = false, length = 20)
+    private String phone;
+
+    @Column(name = "address", nullable = false, length = 255)
+    private String address;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "role", nullable = false)
+    private Role role;
+
+    @CreationTimestamp
+    @Column(name = "created_at", nullable = false, updatable = false)
+    private LocalDateTime createdAt;
+
+    @UpdateTimestamp
+    @Column(name = "updated_at", nullable = false)
+    private LocalDateTime updatedAt;
 }
