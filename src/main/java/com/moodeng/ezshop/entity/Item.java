cab4c9eeb2a3ed20452571a0457c804cfb336cd1
kbs <kim3210542@gmail.com>
2025-11-06 01:06:27 +0900

    Feat: Item 엔티티 정의
    Item 엔티티 기본필드, 제약조건 설정
    
    Resolves: #7
    Ref: #6

--- src/main/java/com/moodeng/ezshop/entity/Item.java
+++ src/main/java/com/moodeng/ezshop/entity/Item.java
@@ -1,13 +1,54 @@
 package com.moodeng.ezshop.entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import com.moodeng.ezshop.constant.ItemStatus;
+import jakarta.persistence.*;
+import lombok.*;
 
 @Entity
-public class Item {
+@Table(name="items")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Item extends BaseEntity{
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "item_id")
     private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @JoinColumn(name = "category_id", nullable = false)
+    private Category category;
+
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @JoinColumn(name = "user_id", nullable = false) // ERD에는 null로 되어있는데 notnull로 함
+    private User user; // 판매자인 user
+
+    @Column(name = "name", nullable = false, length = 120)
+    private String name;
+
+    @Column(name = "origin", length = 50)
+    private String origin;
+
+//    중량은 일단 기입안했음
+//    private Integer weight;
+    
+    @Column(name = "price",  nullable = false)
+    private Integer price;
+
+    @Column(name = "stock_quantity", nullable = false)
+    private Integer stockQuantity;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "item_status", nullable = false)
+    private ItemStatus status;
+
+    @Column(name = "detail_image_url", length = 255)
+    private String detailImageUrl;
+
+    @Column(name = "thumbnail_url", length = 255)
+    private String thumbnailUrl;
+
+
 }
