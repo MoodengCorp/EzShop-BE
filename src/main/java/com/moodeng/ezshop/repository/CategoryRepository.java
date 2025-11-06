be7121c86c95ebcbe293fe7b2f4b8bac70f7834a
kbs <kim3210542@gmail.com>
2025-11-06 23:33:48 +0900

    Feat: Item 도메인 관련 파일 추가
    ItemController, ItemService, ItemRepository, CategoryRepository 스텁파일 추가
    
    Resolves: #6 #9
    Ref:

--- src/main/java/com/moodeng/ezshop/repository/CategoryRepository.java
+++ src/main/java/com/moodeng/ezshop/repository/CategoryRepository.java
@@ -1,9 +1,9 @@
 package com.moodeng.ezshop.repository;
 
-import com.moodeng.ezshop.entity.Item;
+import com.moodeng.ezshop.entity.Category;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface ItemRepository extends JpaRepository<Item,Long> {
+public interface CategoryRepository extends JpaRepository<Category,Long> {
 }
