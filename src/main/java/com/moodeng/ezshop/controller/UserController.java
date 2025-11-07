8ba317406c7e84a59061d419d6bb4f38a19a4fb0
HiTae18 <somu950108@gmail.com>
2025-11-07 15:34:57 +0900

    Feat: User - 회원 가입 기능 구현
    회원관련 기능
    
    ref:#2
    closes:#3

--- src/main/java/com/moodeng/ezshop/controller/UserController.java
+++ src/main/java/com/moodeng/ezshop/controller/UserController.java
@@ -1,4 +1,27 @@
 package com.moodeng.ezshop.controller;
 
+import com.moodeng.ezshop.dto.user.SignupDto;
+import com.moodeng.ezshop.service.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/user")
+@RequiredArgsConstructor
 public class UserController {
+
+    private final UserService userService;
+
+    @PatchMapping("/signup")
+    public ResponseEntity<String> signup(@RequestBody SignupDto signupDto) {
+
+        userService.signup(signupDto);
+
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
 }
