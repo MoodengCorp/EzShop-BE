7032d2d7ca21b48a9eeb366ae1000da2a171e537
sinheetae <somu124@naver.com>
2025-11-07 15:34:57 +0900

    Feat: User - 회원 가입 기능 구현
    회원관련 기능
    
    ref:#2
    closes:#3

--- src/main/java/com/moodeng/ezshop/dto/user/SignupDto.java
+++ src/main/java/com/moodeng/ezshop/dto/user/SignupDto.java
@@ -1,4 +1,22 @@
 package com.moodeng.ezshop.dto.user;
 
+import com.moodeng.ezshop.constant.Role;
+import com.moodeng.ezshop.entity.User;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 public class SignupDto {
+    private String email;
+    private String password;
+    private String name;
+    private String phone;
+    private String address;
+    private Role role;
+
+    //Helper Method
+    public User toEntity() {
+        return User.builder().email(email).password(password).name(name).address(address).role(role).build();
+    }
 }
--- src/main/java/com/moodeng/ezshop/service/UserService.java
+++ src/main/java/com/moodeng/ezshop/service/UserService.java
@@ -1,4 +1,26 @@
 package com.moodeng.ezshop.service;
 
+import com.moodeng.ezshop.dto.user.SignupDto;
+import com.moodeng.ezshop.entity.User;
+import com.moodeng.ezshop.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@RequiredArgsConstructor
 public class UserService {
+    private final UserRepository userRepository;
+
+    @Transactional
+    public void signup(SignupDto signupDto) {
+        // Email Duplicate check
+        if (userRepository.findByEmail(signupDto.getEmail()).isPresent()) {
+            throw new IllegalArgumentException("이미 사용 중인 이메일입니다.");
+        }
+
+        User user = signupDto.toEntity();
+
+        userRepository.save(user);
+    }
 }
