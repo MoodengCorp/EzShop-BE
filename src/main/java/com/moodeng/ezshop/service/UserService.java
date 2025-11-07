9b7ebd3daed736396695bab2ef88b04469b0b148
HiTae18 <somu950108@gmail.com>
2025-11-07 17:50:55 +0900

    Fix: User - 회원 가입 기능 구현
    PasswordEncoder 적용
    
    ref:#2
    closes:#3

--- src/main/java/com/moodeng/ezshop/service/UserService.java
+++ src/main/java/com/moodeng/ezshop/service/UserService.java
@@ -1,9 +1,12 @@
 package com.moodeng.ezshop.service;
 
-import com.moodeng.ezshop.dto.user.SignupDto;
+import com.moodeng.ezshop.dto.request.LoginRequestDto;
+import com.moodeng.ezshop.dto.request.SignupRequestDto;
+import com.moodeng.ezshop.dto.response.LoginResponseDto;
 import com.moodeng.ezshop.entity.User;
 import com.moodeng.ezshop.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -11,15 +14,17 @@ import org.springframework.transaction.annotation.Transactional;
 @RequiredArgsConstructor
 public class UserService {
     private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
 
     @Transactional
-    public void signup(SignupDto signupDto) {
+    public void signup(SignupRequestDto signupDto) {
         // Email Duplicate check
         if (userRepository.findByEmail(signupDto.getEmail()).isPresent()) {
             throw new IllegalArgumentException("이미 사용 중인 이메일입니다.");
         }
+        String encodedPassword = passwordEncoder.encode(signupDto.getPassword());
 
-        User user = signupDto.toEntity();
+        User user = signupDto.toEntity(encodedPassword);
 
         userRepository.save(user);
     }
