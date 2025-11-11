c63828dfd90d624f8044abda7a11050de694dbcb
Yonghoon Lee <ava7890@naver.com>
2025-11-12 00:33:04 +0900

    temp

---
+++ src/main/java/com/moodeng/ezshop/entity/Cart.java
@@ -0,0 +1,28 @@
+package com.moodeng.ezshop.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.ArrayList;
+
+@Entity
+@Table(name="carts")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Cart extends BaseEntity{
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "cart_id")
+    private Long id;
+
+    @OneToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", nullable = false, unique = true)
+    private User user;
+
+//    @OneToMany(mappedBy = "cart", cascade = CascadeType.ALL, orphanRemoval = true)
+//    @Builder.Default
+//    private List<CartItem> items = new ArrayList<>();
+}
